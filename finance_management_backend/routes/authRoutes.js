1806b2a7027f54010ebbccaef6511563f641f4c1
Logesh235 <72260015+Logesh235@users.noreply.github.com>
2025-05-02 20:16:13 +0530

    Delete finance_management_backend directory

--- finance_management_backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController');
-
-// User registration and login
-router.post('/register',registerUser);
-router.post('/login', loginUser);
-
-module.exports = router;
